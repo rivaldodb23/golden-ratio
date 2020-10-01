4129209edf237b34e7601dbd53628266dbf5f2e2
rivaldodb23 <54894364+rivaldodb23@users.noreply.github.com>
2020-10-01 18:03:01 +0200

    Update run.py

--- run.py
+++ run.py
@@ -1,13 +1,25 @@
 import numpy as np
+import math
 
 #--------Enter your function here---------
 def f(x):
     y = x*x
     return y
 
+# ----- Helper function for print ------
+def out(i ,a, x1, x2, b, fx1, fx2):
+    print("Step ", "\ta = ", a, "\tx1 = ", x1, "\tx2 = ", x2, "\t", "\tb = ", b, "\tf(x1) = ", f(x1), "\tf(x2) = ", f(x2))
+
 if __name__ == "__main__":
+    # golden rario
+    psi = 0.5*(1+math.sqrt(5))
+    print("Golden ratio is ", psi)
+
+    # is min/max
+    isMin = True
+
     #-----Enter your accuracy here-----
-    accuracy = 0.01
+    accuracy = 0.001
 
     # lower bound
     a = 0
@@ -16,6 +28,36 @@ if __name__ == "__main__":
     b = 2
 
     # current accuracy/length
-    l = b-a
+    curr_acc = b-a
 
     # Golden ratio part 1:
+    x1 = b - (1/psi) * curr_acc
+    x2 = a + (1/psi) * curr_acc
+
+    i = 3
+    while (curr_acc > accuracy):
+        if (isMin) :
+            if f(x1) < f(x2):
+                b = x2
+                x2 = x1
+                x1 = b - (1/psi) * (b-a)
+                curr_acc = b - x1
+            else:
+                a = x1
+                x1 = x2
+                x2 = a + (1/psi) * (b-a)
+                curr_acc = x2 - a
+        else :
+            if f(x1) >= f(x2):
+                b = x2
+                x2 = x1
+                x1 = b - (1/psi) * (b-a)
+                curr_acc = b - x1
+            else:
+                a = x1
+                x1 = x2
+                x2 = a + (1/psi) * (b-a)
+                curr_acc = x2 - a
+        
+        out(i ,a, x1, x2, b, f(x1), f(x2))
+        i = i + 1
